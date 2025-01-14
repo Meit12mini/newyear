c581c6a95e955c1af62a7ae30b773745a14a72d7
meit <zhora.makarov.2005@yandex.ru>
2025-01-14 11:01:21 +0900

    isi 1

--- 1.ts
+++ 1.ts
@@ -1,1 +1,11 @@
+let a :string= String(prompt('Что повторить'))
+  let c:string = ""
+let bb: number = Number(prompt('Сколько раз?'))
 
+function repeatmy(){
+for (let index = 0; index < bb; index++) { 
+    c += a
+}
+return alert(c)
+}
+repeatmy()
