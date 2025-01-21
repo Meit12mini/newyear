a2bc110b101550040d338d0b4a369d571fddb597
meit <zhora.makarov.2005@yandex.ru>
2025-01-21 14:57:10 +0900

    st6

---
+++ 6.ts
@@ -0,0 +1,15 @@
+const user1 = { name: "Petr", age: 8 };
+const user2 = { name: "Petros", age: 11 };
+
+function agetest(user1: any, user2: any) {
+  if (user1.age == user2.age) {
+    return null;
+  }
+  if (user1.age > user2.age) {
+    return user1;
+  } else {
+    return user2;
+  }
+}
+
+console.log(agetest(user1, user2));
